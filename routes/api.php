659ac71f10ef00cd545d477fe232afa73a2d00ba
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-18 15:31:47 +0200

    Added cors middleware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\PartyController;
+// use App\Http\Controllers\GameController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,39 +18,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 // postman -> http://127.0.0.1:8000/api/nombreEndPoint
 
-// RF.1 Register
-Route::post('/register', [PlayerController::class, 'registerPlayer']);
+Route::group(['middleware'=> 'cors'], function () {
 
-// RF.2 LogIn
-Route::post('/login', [PlayerController::class, 'loginPlayer']);
+    // RF.1 Register
+    Route::post('/register', [PlayerController::class, 'registerPlayer']);
 
-// RF.3 Crear party
-Route::post('/createParty', [PartyController::class, 'createParty']);
+    // RF.2 LogIn
+    Route::post('/login', [PlayerController::class, 'loginPlayer']);
 
-// RF.4 Buscar party
-Route::get('/searchParty/{gameName}', [PartyController::class, 'searchPartyGameName']);
+    // RF.3 Crear party
+    Route::post('/createParty', [PartyController::class, 'createParty']);
 
-// RF.5 Entrar party
+    // RF.4 Buscar party
+    Route::get('/searchParty/{gameName}', [PartyController::class, 'searchPartyGameName']);
 
-
-// RF.5.2 Salir party
-
-
-// RF.6 Mensajes party
+    // RF.5 Entrar party
 
 
-// RF.7 Traer mensajes party
-Route::get('/allMessages', [PartyController::class, 'countMessages']);
-
-// RF.8 Modificar datos perfil
-Route::post('/modify/{username}', [PlayerController::class, 'modifyUsername']);
+    // RF.5.2 Salir party
 
 
-// RF.9 LogOut
-Route::post('/logout', [PlayerController::class, 'logOut'])->middleware('token');
+    // RF.6 Mensajes party
+
+
+    // RF.7 Traer mensajes party
+    Route::get('/allMessages', [PartyController::class, 'countMessages']);
+
+    // RF.8 Modificar datos perfil
+    Route::post('/modify/{username}', [PlayerController::class, 'modifyUsername']);
+
+
+    // RF.9 LogOut
+    Route::post('/logout', [PlayerController::class, 'logOut'])->middleware('token');
+
+
+});
