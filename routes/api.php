1074d2064bdb557eb03c517b3861b470098e5e8c
Kronapsys <rsxvii@gmail.com>
2021-04-18 12:02:07 +0200

    added routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,38 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// postman -> http://127.0.0.1:8000/api/...
+
+// RF.1 Register
+Route::post('/register', [PlayerController::class, 'registerPlayer']);
+
+// RF.2 LogIn
+Route::post('/login', [PlayerController::class, 'loginPlayer']);
+
+// RF.3 Crear party
+Route::post('/createParty', [PartyController::class, 'createParty']);
+
+// RF.4 Buscar party
+Route::get('/searchParty/{gameName}', [PartyController::class, 'searchPartyGameName']);
+
+// RF.5 Entrar party
+
+
+// RF.5.2 Salir party
+
+
+// RF.6 Mensajes party
+
+
+// RF.7 Traer mensajes party
+Route::get('/allMessages', [PartyController::class, 'countMessages']);
+
+// RF.8 Modificar datos perfil
+Route::post('/modify/{username}', [PlayerController::class, 'modifyUsername']);
+
+
+// RF.9 LogOut
+Route::post('/logout', [PlayerController::class, 'logOut'])->middleware('token');
+
+
